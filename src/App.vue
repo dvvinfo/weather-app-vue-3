10fb4407926a79473e3eb4182814ae27773b6b27
dvvinfo <dvvinfo@mail.ru>
2023-02-04 16:58:39 +0300

    dded page headers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <nav-bar />
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="page" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -10,4 +14,11 @@ import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.page-enter-active {
+  transition: 600ms ease all;
+}
+.page-enter-from {
+  opacity: 0;
+}
+</style>
